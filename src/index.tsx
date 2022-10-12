cff164fd924ae6cd15d4019d05492cb242313639
BrunoGaruda <bruleorn@gmail.com>
2022-10-12 04:03:32 -0300

    Configuring a new transaction and store in miragejs schema data

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App' // Ao retirar o default no App, tive que colocar chaves
 
 createServer({
+  models: {
+    // https://miragejs.com/api/classes/model/
+    transaction: Model
+  },
+
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions ', () => {
-      return [
-        {
-          namespace: 'api',
-          id: 1,
-          title: 'transactions',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      //
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      // necessário usar o parse para converter o objeto de texto para javascript
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
