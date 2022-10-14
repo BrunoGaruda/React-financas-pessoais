619164d3bb3041854ff2640397d48c036f2d0009
BrunoGaruda <bruleorn@gmail.com>
2022-10-13 23:32:48 -0300

    using useContext hook to render data from summary and transactionsTable

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,15 @@
+import React, { useContext } from 'react'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { TransactionsContext } from '../../TransactionsContex'
 
 import { Container } from './styles'
 
 export function Summary() {
+  // Utilizando o contexto com Hook {useContext}
+  const data = useContext(TransactionsContext)
+
   return (
     <Container>
       <div>
